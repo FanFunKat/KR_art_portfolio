eea303960cfe287ce3193a822ea8a4a7623d8a10
Katarzyna Russek <katarzyna.russek@gmail.com>
2024-01-06 17:49:39 +0100

    added 'photoInfo' module with photo data

--- app.js
+++ app.js
@@ -8,48 +8,50 @@ const buyBtn = document.getElementById('formBuy');
 const inputImage = document.querySelector('.form__img')
 const headerCard = document.querySelector('.form__title')
 
-const galery = {
-  wedding: {
-    title: 'The wedding',
-    hash: ['#couple', '#bride', '#groom'],
-    price: 10.99,
-  },
-  children: {
-    title: 'The portrait of children',
-    hash: ['#siblings', '#family', '#smile'],
-    price: 10.99,
-  },
-  city: {
-    title: 'The city in gray',
-    hash: ['#architecture', '#buildings', '#detail'],
-    price: 10.99,
-  },
-  wild: {
-    title: 'The Woman with the Tiger',
-    hash: ['#wild', '#tiger', '#nature'],
-    price: 10.99,
-  },
-  landscape: {
-    title: 'Japanese garden',
-    hash: ['#Japan', '#garden', '#view'],
-    price: 10.99,
-  },
-  nature: {
-    title: 'In nature',
-    hash: ['#happy', '#nature', '#relax'],
-    price: 10.99,
-  },
-  portrait: {
-    title: 'The portrait of a redhead girl',
-    hash: ['#redhead', '#jasmine', '#beauty'],
-    price: 10.99,
-  },
-  pets: {
-    title: 'Cat',
-    hash: ['#cat', '#animal', '#cute'],
-    price: 10.99,
-  },
-};
+// const galery = {
+//   wedding: {
+//     title: 'The wedding',
+//     hash: ['#couple', '#bride', '#groom'],
+//     price: 10.99,
+//   },
+//   children: {
+//     title: 'The portrait of children',
+//     hash: ['#siblings', '#family', '#smile'],
+//     price: 10.99,
+//   },
+//   city: {
+//     title: 'The city in gray',
+//     hash: ['#architecture', '#buildings', '#detail'],
+//     price: 10.99,
+//   },
+//   wild: {
+//     title: 'The Woman with the Tiger',
+//     hash: ['#wild', '#tiger', '#nature'],
+//     price: 10.99,
+//   },
+//   landscape: {
+//     title: 'Japanese garden',
+//     hash: ['#Japan', '#garden', '#view'],
+//     price: 10.99,
+//   },
+//   nature: {
+//     title: 'In nature',
+//     hash: ['#happy', '#nature', '#relax'],
+//     price: 10.99,
+//   },
+//   portrait: {
+//     title: 'The redhead girl',
+//     hash: ['#redhead', '#jasmine', '#beauty'],
+//     price: 10.99,
+//   },
+//   pets: {
+//     title: 'Cat',
+//     hash: ['#cat', '#animal', '#cute'],
+//     price: 10.99,
+//   },
+// };
+
+import { galery } from './photoInfo.js';
 
 const inputTitle = document.getElementById('formTitle');
 const inputPrice = document.getElementById('formPrice');
---
+++ photoInfo.js
@@ -0,0 +1,42 @@
+export const galery = {
+  wedding: {
+    title: 'The wedding',
+    hash: ['#couple', '#bride', '#groom'],
+    price: 10.99,
+  },
+  children: {
+    title: 'The portrait of children',
+    hash: ['#siblings', '#family', '#smile'],
+    price: 10.99,
+  },
+  city: {
+    title: 'The city in gray',
+    hash: ['#architecture', '#buildings', '#detail'],
+    price: 10.99,
+  },
+  wild: {
+    title: 'The Woman with the Tiger',
+    hash: ['#wild', '#tiger', '#nature'],
+    price: 10.99,
+  },
+  landscape: {
+    title: 'Japanese garden',
+    hash: ['#Japan', '#garden', '#view'],
+    price: 10.99,
+  },
+  nature: {
+    title: 'In nature',
+    hash: ['#happy', '#nature', '#relax'],
+    price: 10.99,
+  },
+  portrait: {
+    title: 'The redhead girl',
+    hash: ['#redhead', '#jasmine', '#beauty'],
+    price: 10.99,
+  },
+  pets: {
+    title: 'Cat',
+    hash: ['#cat', '#animal', '#cute'],
+    price: 10.99,
+  },
+};
