465cacf138f1bbbe34631cdcd195001aa99126b2
Katarzyna Russek <katarzyna.russek@gmail.com>
2024-01-29 18:24:23 +0100

    added component 'LandingPage'

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css'
 import { Gallery } from './components/Gallery/Gallery'
+import { LandingPage } from './components/LandingPage/LandingPage'
 
 function App() {
   return (
     <>
+      <LandingPage />
       <Gallery />
     </>
   )
---
+++ my-app/src/components/LandingPage/LandingPage.jsx
@@ -0,0 +1,10 @@
+export const LandingPage = () => {
+  return (
+    <section className='{styles.landingPage__box}'>
+      <h1 className='{styles.landingPage_title}'>
+        Your <b>moments</b><br />My <b>passion</b><br />Our <b>story</b>
+      </h1>
+      <img className='{styles.landingPage__img}' src="image/girl-2436545_1280.jpg" alt="example image" />
+    </section>
+  )
+}
