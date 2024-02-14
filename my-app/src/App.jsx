9a48509dc0be56b06ed58c3e9dd63e71f7f140e7
Katarzyna Russek <katarzyna.russek@gmail.com>
2024-02-14 16:30:16 +0100

    Added structure to FormImage and App

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,12 +1,18 @@
 import './App.css'
+import { Form } from './components/Form/Form'
+import { FormImage } from './components/FormImage/FormImage'
 import { Gallery } from './components/Gallery/Gallery'
 import { LandingPage } from './components/LandingPage/LandingPage'
+import { Pricing } from './components/Pricing/Pricing'
 
 function App() {
   return (
     <>
       <LandingPage />
       <Gallery />
+      <Pricing />
+      <FormImage />
+      <Form />
     </>
   )
 }
