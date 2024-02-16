649cb81269943af1a3560ffb3ae5057d411a8535
Katarzyna Russek <katarzyna.russek@gmail.com>
2024-02-16 10:37:55 +0100

    added module.css to FormImage

--- my-app/src/components/FormImage/FormImage.jsx
+++ my-app/src/components/FormImage/FormImage.jsx
@@ -1,18 +1,20 @@
+import { styles } from './FormImage.module.css'
+
 export const FormImage = () => {
 
   return (
-    <form action="javascript:;" id="productCard" className="product__card">
-      <input type="image" alt="cat" src="#" className="form__img" />
-      <div className="form_card">
-        <div className="form_information">
-          <button className="form__close--btn"><i className="fa-solid fa-xmark form-close"></i></button>
-          <h2 id="formTitle" className="form__title">Title</h2>
-          <p id="formPrice" className="form__price">$10</p>
-          <span id="formHash1" className="form__hash">#tag1</span>
-          <span id="formHash2" className="form__hash">#tag2</span>
-          <span id="formHash3" className="form__hash">#tag3</span>
+    <form action="javascript:;" id="productCard" className={styles.product__card}>
+      <input type="image" alt="cat" src="#" className={styles.form__img} />
+      <div className={styles.form_card}>
+        <div className={styles.form_information}>
+          <button className={styles.form__closeBtn}><i className="fa-solid fa-xmark form-close"></i></button>
+          <h2 id="formTitle" className={styles.form__title}>Title</h2>
+          <p id="formPrice" className={styles.form__price}>$10</p>
+          <span id="formHash1" className={styles.form__hash}>#tag1</span>
+          <span id="formHash2" className={styles.form__hash}>#tag2</span>
+          <span id="formHash3" className={styles.form__hash}>#tag3</span>
         </div>
-        <button type="submit" id="formBuy" className="form__buy">Add to basket</button>
+        <button type="submit" id="formBuy" className={styles.form__buy}>Add to basket</button>
       </div>
     </form>
   )
