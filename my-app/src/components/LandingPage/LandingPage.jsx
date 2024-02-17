24009a6eae46eaf1c06ce7f2a24ba002659c5850
Katarzyna Russek <katarzyna.russek@gmail.com>
2024-02-17 10:33:38 +0100

    added module.css to LandingPage

--- my-app/src/components/LandingPage/LandingPage.jsx
+++ my-app/src/components/LandingPage/LandingPage.jsx
@@ -1,10 +1,12 @@
+import { styles } from './LandingPage.module.css'
+
 export const LandingPage = () => {
   return (
-    <section className='{styles.landingPage__box}'>
-      <h1 className='{styles.landingPage_title}'>
+    <section className={styles.landingPage__box}>
+      <h1 className={styles.landingPage_title}>
         Your <b>moments</b><br />My <b>passion</b><br />Our <b>story</b>
       </h1>
-      <img className='{styles.landingPage__img}' src="image/girl-2436545_1280.jpg" alt="example image" />
+      <img className={styles.landingPage__img} src="image/girl-2436545_1280.jpg" alt="example image" />
     </section>
   )
 }
