8be396ca51717a1c3a7daa2aa2eb3d311a6dfa9c
Katarzyna Russek <katarzyna.russek@gmail.com>
2024-02-19 13:19:33 +0100

    landingPage - useState & useEffect - changing slogan

--- my-app/src/components/LandingPage/LandingPage.jsx
+++ my-app/src/components/LandingPage/LandingPage.jsx
@@ -1,10 +1,49 @@
+import { useEffect, useState } from 'react';
 import styles from './LandingPage.module.css'
 
 export const LandingPage = () => {
+
+  const [slogan, setSlogan] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setSlogan(prevSlogan => (prevSlogan + 1) % 3);
+    }, 2000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  const getSloganPart = () => {
+    switch (slogan) {
+      case 0:
+        return (
+          <>
+            Your <b>moments</b>
+          </>
+        );
+      case 1:
+        return (
+          <>
+            My <b>passion</b>
+          </>
+        );
+      case 2:
+        return (
+          <>
+            Our <b className={styles.last__b}>story</b>
+          </>
+        );
+      default:
+        return '';
+    }
+  };
+
   return (
     <section className={styles.landingPage__box}>
       <h1 className={styles.landingPage_title}>
-        Your <b>moments</b><br />My <b>passion</b><br />Our <b>story</b>
+        {getSloganPart()}
       </h1>
       <img className={styles.landingPage__img} src="image/girl-2436545_1280.jpg" alt="example image" />
     </section>
