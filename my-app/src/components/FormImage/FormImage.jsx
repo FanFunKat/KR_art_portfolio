22a58aa8183fa20fcb77666c89486056e7d77f65
Katarzyna Russek <katarzyna.russek@gmail.com>
2024-02-21 23:13:47 +0100

    selected image - card product :)

--- my-app/src/components/FormImage/FormImage.jsx
+++ my-app/src/components/FormImage/FormImage.jsx
@@ -2,25 +2,28 @@ import styles from './FormImage.module.css'
 import { useOutletContext, useParams } from 'react-router';
 
 export const FormImage = () => {
+
   let { id } = useParams();
   const gallery = useOutletContext();
-  const selectedImage = gallery.find(image => image.id === id);
+  const selectedImage = gallery.find(image => image.id == id);
 
   if (!selectedImage) {
     return <div>Loading...</div>;
   }
 
+  // console.log(selectedImage);
+
   return (
     <form className={styles.product__card}>
-      <input type="image" alt={selectedImage.title} src={`/public/${selectedImage.src}`} key={selectedImage.id} className={styles.form__img} />
+      <input type="image" alt={selectedImage.title} src={`/${selectedImage.src}`} key={selectedImage.id} className={styles.form__img} />
       <div className={styles.form_card}>
         <div className={styles.form_information}>
           <button className={styles.form__closeBtn}><i className="fa-solid fa-xmark form-close"></i></button>
-          <h2 className={styles.form__title}>Title</h2>
-          <p className={styles.form__price}>$10</p>
-          <span className={styles.form__hash}>#tag1</span>
-          <span className={styles.form__hash}>#tag2</span>
-          <span className={styles.form__hash}>#tag3</span>
+          <h2 className={styles.form__title}>{selectedImage.title}</h2>
+          <p className={styles.form__price}>${selectedImage.price}</p>
+          <span className={styles.form__hash}>{selectedImage.tag1}</span>
+          <span className={styles.form__hash}>{selectedImage.tag2}</span>
+          <span className={styles.form__hash}>{selectedImage.tag3}</span>
         </div>
         <button type="submit" className={styles.form__buy}>Add to basket</button>
       </div>
