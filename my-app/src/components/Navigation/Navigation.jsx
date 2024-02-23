a09badc3986d76daa7301469d257069d4f6ffaa3
Katarzyna Russek <katarzyna.russek@gmail.com>
2024-02-23 15:33:07 +0100

    added NavLink

--- my-app/src/components/Navigation/Navigation.jsx
+++ my-app/src/components/Navigation/Navigation.jsx
@@ -1,20 +1,21 @@
+import { NavLink } from 'react-router-dom';
 import styles from './Navigation.module.css'
 
 export const Navigation = () => {
   return (
     <header className={styles.header}>
       <div className={styles.header__box}>
-        <a href="/" className={styles.logo__box}>
+        <NavLink to="/" className={styles.logo__box}>
           <div className={styles.logo}>
             <img className={styles.logo__img} src="/image/example.svg" alt="logo image example" />
             <p className={styles.logo__name}>PhotoToTo</p>
           </div>
-        </a>
+        </NavLink>
         <nav className={styles.nav}>
           <ul className={styles.nav__items}>
-            <li><a href="/gallery" className={styles.nav__itemsLink}>Gallery</a></li>
-            <li><a href="/pricing" className={styles.nav__itemsLink}>Pricing</a></li>
-            <li><a href="/contact" className={styles.nav__itemsLink}>Contact</a></li>
+            <li><NavLink to="/gallery" className={styles.nav__itemsLink}>Gallery</NavLink></li>
+            <li><NavLink to="/pricing" className={styles.nav__itemsLink}>Pricing</NavLink></li>
+            <li><NavLink to="/contact" className={styles.nav__itemsLink}>Contact</NavLink></li>
           </ul>
         </nav>
       </div>
